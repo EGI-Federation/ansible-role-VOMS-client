b7aa75d45c08d27143e6ccd24d115c71467f2a80
Bruce Becker <brucellino@gmail.com>
2018-05-31 16:18:50 +0200

    Add test and task on packages

--- molecule/default/tests/test_voms.py
+++ molecule/default/tests/test_voms.py
@@ -1,14 +1,25 @@
 import os
 import testinfra.utils.ansible_runner
+import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_voms_dir(host):
-    vomses_dir = host.file('/etc/vomses')
-    voms_dir = host.file('/etc/grid-security/vomsdir')
+@pytest.mark.parametrize("OS","package", 
+    [("centos6"),("voms","fetch-crl")]
+)
+
+def test_packages(host, package):
+    assert host.package(package).is_installed
+
+
+@pytest.mark.parametrize("directory",
+    [("/etc/vomses"), "/etc/grid-security/vomsdir"]
+)
+def test_voms_dir(host, directory):
     
-    assert vomses_dir.exists
+    assert host.file(directory).exists
+    assert host.file(directory).is_directory
 
 
 
