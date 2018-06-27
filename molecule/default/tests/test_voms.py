ce7ccff5bfd1ccac14779b222d75b8cbffadd822
Bruce Becker <brucellino@gmail.com>
2018-06-27 06:39:28 +0200

    Add failing tests on VOMSes file assertions

--- molecule/default/tests/test_voms.py
+++ molecule/default/tests/test_voms.py
@@ -14,9 +14,28 @@ def test_voms_dir(host, directory):
     assert host.file(directory).is_directory
 
 
-@pytest.mark.parametrize("voname", [
-    "ops",
-    "dteam"])
-def test_vo_data(host, voname):
+# Check that sample VO data is correct
+@pytest.mark.parametrize("voname,voms_server,dn,ca_dn", [
+    ("ops",
+     "lcg-voms2.cern.ch",
+     "'/DC=ch/DC=cern/OU=computers/CN=lcg-voms2.cern.ch'",
+     "'/DC=ch/DC=cern/CN=CERN Grid Certification Authority'"),
+    ("dteam",
+     "voms2.hellasgrid.gr",
+     "'/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr'",
+     "'/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2016'"),
+    ("demo.fedcloud.egi.eu",
+     "voms1.grid.cesnet.cz",
+     "'/DC=cz/DC=cesnet-ca/O=CESNET/CN=voms1.grid.cesnet.cz'",
+     "'/DC=cz/DC=cesnet-ca/O=CESNET CA/CN=CESNET CA 3'"),
+    ("fedcloud.egi.eu",
+     "voms1.grid.cesnet.cz",
+     "'/DC=cz/DC=cesnet-ca/O=CESNET/CN=voms1.grid.cesnet.cz'",
+     "'/DC=cz/DC=cesnet-ca/O=CESNET CA/CN=CESNET CA 3'")
+])
+def test_vo_configuration(host, voname, voms_server, dn, ca_dn):
+    vomses_filename = '/etc/vomses/' + voname + '-' + voms_server
+    vomses_file = host.file(vomses_filename)
 
-    assert True
+    assert vomses_file.exists
+    # assert vomses_file.contains(/voname/)
