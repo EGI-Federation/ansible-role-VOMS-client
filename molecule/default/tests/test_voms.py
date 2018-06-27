25272628701f7f1378ea837d8f9cfd24667d3e21
Bruce Becker <brucellino@gmail.com>
2018-06-27 16:47:35 +0200

    WIP on #7

--- molecule/default/tests/test_voms.py
+++ molecule/default/tests/test_voms.py
@@ -38,25 +38,21 @@ def test_voms_dir(host, directory):
 
 
 # Check that sample VO data is correct
-@pytest.mark.parametrize("voname,voms_server,dn,ca_dn", [
+@pytest.mark.parametrize("voname,voms_server,dn", [
     ("ops",
      "lcg-voms2.cern.ch",
-     '/DC=ch/DC=cern/OU=computers/CN=lcg-voms2.cern.ch',
-     '/DC=ch/DC=cern/CN=CERN Grid Certification Authority'),
+     '/DC=ch/DC=cern/OU=computers/CN=lcg-voms2.cern.ch'),
     ("dteam",
      "voms2.hellasgrid.gr",
-     '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr',
-     '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2016'),
+     '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr'),
     ("demo.fedcloud.egi.eu",
      "voms1.grid.cesnet.cz",
-     '/DC=cz/DC=cesnet-ca/O=CESNET/CN=voms1.grid.cesnet.cz',
-     '/DC=cz/DC=cesnet-ca/O=CESNET CA/CN=CESNET CA 3'),
+     '/DC=cz/DC=cesnet-ca/O=CESNET/CN=voms1.grid.cesnet.cz'),
     ("fedcloud.egi.eu",
      "voms1.grid.cesnet.cz",
-     '/DC=cz/DC=cesnet-ca/O=CESNET/CN=voms1.grid.cesnet.cz',
-     '/DC=cz/DC=cesnet-ca/O=CESNET CA/CN=CESNET CA 3')
+     '/DC=cz/DC=cesnet-ca/O=CESNET/CN=voms1.grid.cesnet.cz')
 ])
-def test_vo_configuration(host, voname, voms_server, dn, ca_dn):
+def test_vo_configuration(host, voname, voms_server, dn):
     vomses_filename = '/etc/vomses/' + voname + '-' + voms_server
     vomses_file = host.file(vomses_filename)
     assert vomses_file.exists
