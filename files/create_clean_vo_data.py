4459fc9603ec8fc1fbf85bd80ff734c036d67694
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-10-20 09:39:04 +0200

    Misc updates (#23)
    * README: format with prettier, little fixes
    * README: fix example
    * Remove unused import
    * Update shebang, mark as executable
    * requirements.txt: pymarkdownlint is unmaintained and was used for deprecated travis checks
    * requirements.txt: add dependencies for create_clean_vo_data.py
    * Ignore generated python venv files
    * main: update API endpoint
    * create_clean_vo_data.py: clean imports, load conf relatively
    * create_clean_vo_data.py: update request to use new API
    * README: document updating VO data
    * create_clean_vo_data.py: write file in proper directory
    * create_clean_vo_data.py: run prettier
    * create_clean_vo_data.py: document get_data
    * create_clean_vo_data.py: update doc for filter_data
    * create_clean_vo_data.py: adapt to new format, non VOMS registries got included
    * Re-generate data.yml
    
    There are many differences, but now non VOMS VO are not documented, same for VO leaving or decommissioned.

--- files/create_clean_vo_data.py
+++ files/create_clean_vo_data.py
@@ -1,21 +1,45 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 from __future__ import print_function
 
 import json
+import os
+import pathlib
+import sys
+
+import jsbeautifier
 import requests
 from ruamel import yaml
-import jsbeautifier
 
 
 def get_data():
-    vars = yaml.safe_load(open('../defaults/main.yml'))
-    url = ''.join((vars['lavoisier']['base_url'],
-                   vars['lavoisier']['vo_id_card_endpoint']))
+    """
+    Download VO info data using Operations Portal API
+    """
+    vars = yaml.safe_load(
+        open("%s/../defaults/main.yml" % pathlib.Path(__file__).parent.absolute())
+    )
+    url = "".join(
+        (vars["lavoisier"]["base_url"], vars["lavoisier"]["vo_id_card_endpoint"])
+    )
     try:
-        data = requests.get(url).json()
+        auth_token = os.environ["OPS_PORTAL_API_TOKEN"]
+    except KeyError:
+        print("OPS_PORTAL_API_TOKEN not found in the environment, exiting.")
+        sys.exit(1)
+    headers = {"X-API-Key": auth_token, "Accept": "application/json"}
+    try:
+        response = requests.get(url, headers=headers)
+        status_code = response.status_code
+        if status_code != requests.codes.ok:
+            print("Graph API call result: ")
+            print(status_code)
+            print(json.dumps(response.json(), indent=2))
+            sys.exit(1)
+        data = response.json()
     except UserWarning as e:
         print(e)
+        sys.exit(1)
     return data
 
 
@@ -26,35 +50,50 @@ def filter_data(data):
     entries in it, extracting the relevant information for the VO.
     The result is an object (data) with an array of dicts containing:
         VO name
-        voms server hostname
-        voms port
+        VOMS server hostname
+        VOMS port
         VOMS Server cert DN
         CA DN of the issueing CA
         :param data=vo: the json object to parse
     """
     cleaned_data = {"data": []}
-    for i, vo in enumerate(data['voVoms']):
-        for k, vomses in enumerate(vo['Vo']):
+    for vo in data["results"]:
+        vo_name = vo["name"]
+        clean_vo_info = {"name": vo_name, "voms": []}
+        for vo_info in vo["Vo"]:
+            vo_registry = vo_info["Registries"][0]
             try:
-                clean_vo = {
-                    'name': None,
-                    'voms': {},
-                }
-                voms_server = vo['Vo'][k]['VoVomsServer'][0]['VoVomsServer'][2]
-                clean_vo['voms'].update({
-                    'DN': voms_server['X509Cert'][0]['DN'][0],
-                    'CA_DN': voms_server['X509Cert'][1]['CA_DN'][0],
-                    'hostname': voms_server['host'],
-                    'port': vo['Vo'][k]['VoVomsServer'][0]['vomses_port'],
-                })
-                clean_vo['name'] = vo['name']
-                cleaned_data['data'].append(clean_vo)
-            except IndexError:
-                print("VO %s is bad" % vo['name'])
-    print("%d vos configured" % i)
+                # Only look for VOMS admin server
+                if vo_registry["is_vomsadmin_server"] == "1":
+                    try:
+                        port = vo_registry["vomses_port"]
+                        voms_server = vo_registry["VoVomsServer"]
+                        hostname = voms_server[1]["hostname"][0]
+                        dn = voms_server[2]["X509Cert"][0]["DN"][0]
+                        ca_dn = voms_server[2]["X509Cert"][1]["CA_DN"][0]
+                        clean_vo_info["voms"].append(
+                            {
+                                "DN": dn,
+                                "CA_DN": ca_dn,
+                                "hostname": hostname,
+                                "port": port,
+                            }
+                        )
+                    except IndexError:
+                        print("VOMS for VO %s is bad" % vo_name)
+            except KeyError:
+                # is_vomsadmin_server is not always present
+                pass
+        # Only add VOs having at least one VOMS server
+        if len(clean_vo_info["voms"]) > 0:
+            cleaned_data["data"].append(clean_vo_info)
+        else:
+            print("%s is not having any VOMS server" % vo_name)
+
+    print("%d vos configured" % len(cleaned_data["data"]))
 
     # write it to a file
-    with open('data.yml', 'w') as file:
+    with open("%s/data.yml" % pathlib.Path(__file__).parent.absolute(), "w") as file:
         yaml.dump(cleaned_data, file, Dumper=yaml.RoundTripDumper)
     return 0
 
